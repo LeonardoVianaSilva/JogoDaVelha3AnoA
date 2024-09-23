fc01a04af5211082fb96b49f5c824ba143021afc
LeonardoVianaSilva <00001116010288sp@al.educacao.sp.gov.br>
2024-09-23 13:20:02 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,29 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <main>
-    <h1>JOGO DA VELHA</h1>
-    <hr />
-    <div class="game">
-      <button data-i="1"></button>
-      <button data-i="2"></button>
-      <button data-i="3"></button>
-      <button data-i="4"></button>
-      <button data-i="5"></button>
-      <button data-i="6"></button>
-      <button data-i="7"></button>
-      <button data-i="8"></button>
-      <button data-i="9"></button>
-    </div>
-    <h2 class="currentPlayer"></h2>
-  </main>
-  <script src="script.js"></script>
-</body>
-</html>
+const currentPlayer = document.querySelector(".currentPlayer");
+
+let selected;
+let player = "X";
+
+let positions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function init() {
+  selected = [];
+
+  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+
+  document.querySelectorAll(".game button").forEach((item) => {
+    item.innerHTML = "";
+    item.addEventListener("click", newMove);
+  });
+}
+
+init();
+
+function newMove(e) {
+  const index = e.target.getAttribute("data-i");
+  e.target.innerHTML = player;
+  e.target.removeEventListener("click", newMove);
+  selected[index] = player;
+
+  setTimeout(() => {
+    check();
+  }, [100]);
+
+  player = player === "X" ? "O" : "X";
+  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+}
+
+function check() {
+  let playerLastMove = player === "X" ? "O" : "X";
+
+  const items = selected
+    .map((item, i) => [item, i])
+    .filter((item) => item[0] === playerLastMove)
+    .map((item) => item[1]);
+
+  for (pos of positions) {
+    if (pos.every((item) => items.includes(item))) {
+      alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
+      init();
+      return;
+    }
+  }
+
+  if (selected.filter((item) => item).length === 9) {
+    alert("DEU EMPATE!");
+    init();
+    return;
+  }
+}
